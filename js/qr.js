fef363efd17e488dfa2859c9764e0f4c82d59f1b
Cris Nguyen <hoailuan0311@gmail.com>
2025-12-07 17:21:30 +0700

    Add QR code decoding function from image

---
+++ js/qr.js
@@ -0,0 +1,25 @@
+async function decodeQRFromImage(file) {
+    return new Promise((resolve) => {
+        const img = new Image();
+        img.src = URL.createObjectURL(file);
+
+        img.onload = () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+
+            const w = img.width;
+            const h = img.height;
+
+            // Lấy 25% trên + 25% phải
+            canvas.width = w * 0.5;
+            canvas.height = h * 0.4;
+
+            ctx.drawImage(img, w * 0.5, 0, w * 0.5, h * 0.4, 0, 0, w * 0.5, h * 0.4);
+
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+            const code = jsQR(imageData.data, canvas.width, canvas.height);
+            resolve(code ? code.data : null);
+        };
+    });
+}
