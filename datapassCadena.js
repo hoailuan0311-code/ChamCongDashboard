2d34e4b3fe5f1a9526f511f0ea11ee6835d7a8bc
crisnguyen <hoailuan0311@gmail.com>
2025-12-10 17:01:26 +0700

    Update datapassCadena.js

--- datapassCadena.js
+++ datapassCadena.js
@@ -1,14 +1,1 @@
-const passCadena = [
-  {
-    "Date": "10-Dec",
-    "MSNV": "523768",
-    "HoTen": "Nguyễn Hoài Luận",
-    "BU": "OP",
-    "BoPhan": "Warehouse",
-    "LyDo": "Bị Khóa",
-    "MatKhauMoi": "ABC123!",
-    "Team": "RYOBI",
-    "NgayCapLai": "2025-12-10",
-    "Key": "LUAN001"
-  }
-];
+[{"Date":"46001","MSNV":"523768","HoTen":"Nguyễn Hoài Luận","BU":"OP","BoPhan":"Warehouse","LyDo":"Bị Khóa","MatKhauMoi":"Cris","Team":"Sharp Lu Hao Lin","NgayCapLai":"46001","Key":"Cris"}]
