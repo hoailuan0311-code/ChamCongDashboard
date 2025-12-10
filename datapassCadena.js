b7e95fcb25ed2428f35ddd813076210760a4c7ec
Cris Nguyen <hoailuan0311@gmail.com>
2025-12-10 16:35:07 +0700

    Create datapassCadena.js

---
+++ datapassCadena.js
@@ -0,0 +1,13 @@
+const passCadena = [
+  {
+    "Date": "10-Dec",
+    "MSNV": "523768",
+    "HoTen": "Nguyễn Hoài Luận",
+    "BU": "OP",
+    "BoPhan": "Warehouse",
+    "LyDo": "Bị Khóa",
+    "MatKhauMoi": "ABC123!",
+    "Team": "RYOBI",
+    "Key": "LUAN123"
+  }
+];
