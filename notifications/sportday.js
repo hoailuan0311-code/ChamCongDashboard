76994d6ae5d8df79cca590aed945c008b84b1de9
crisnguyen <hoailuan0311@gmail.com>
2026-06-02 10:57:11 +0700

    Create sportday.js

---
+++ notifications/sportday.js
@@ -0,0 +1,42 @@
+{
+"popupTitle":"🏆 GP SPORT DAY 2026",
+
+
+"nextMatch":{
+    "title":"⚽ Bóng đá Nam",
+    "team":"RYOBI 1F",
+    "datetime":"2026-06-15 08:00:00"
+},
+
+"ticker":[
+    "🏆 GP SPORT DAY 2026",
+    "⚽ Bóng đá Nam: RYOBI 1F",
+    "🏐 Bóng chuyền Nam: Warehouse JIT & Warehouse 3F",
+    "🏸 Trần Quang Trung - Phạm Thị Thu",
+    "♟️ Nguyễn Hoài Luận",
+    "🏃 Việt dã Warehouse",
+    "🎮 Liên Quân Warehouse JIT",
+    "🤝 Kéo Co Warehouse JIT"
+],
+
+"slides":[
+
+    {
+        "title":"⚽ BÓNG ĐÁ NAM",
+        "content":"RYOBI 1F"
+    },
+
+    {
+        "title":"🏐 BÓNG CHUYỀN NAM",
+        "content":"Warehouse JIT<br>Warehouse 3F"
+    },
+
+    {
+        "title":"🏸 CẦU LÔNG",
+        "content":"593662 - Trần Quang Trung<br>511586 - Phạm Thị Thu"
+    }
+
+]
+
+
+}
